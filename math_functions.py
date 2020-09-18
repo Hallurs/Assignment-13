d1aecad82f24960a589f4ae461e60ec8c814a19c
Hallur Hermannsson Aspar <hallurh@7und.is>
2020-09-18 11:27:40 +0000

    math_functions.py

--- math_functions.py
+++ math_functions.py
@@ -6,4 +6,16 @@ def sum_natural(n_str):
         sum_of_int += i
     return sum_of_int
 
-print(sum_natural(30))
+def sum_fibonacci(n_str):
+    ''' Sums up the fibonacci numbers higher or equal too 2'''
+    d = 0
+    a = 0
+    b = 1
+    sum_of_fibonacci = 0
+    for i in range(2,n_str):
+        d = a + b
+        a = b
+        b = d
+        sum_of_fibonacci += d
+    return sum_of_fibonacci
+print(sum_fibonacci(7))
